d1194c51dfa25aa8ec76bdd59207544a7e82c20d
PolarisBBB <deniska1597@yandex.ru>
2026-07-10 17:18:44 +1000

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,12 +1,12 @@
 def format_reservation(data: dict, res_type: str, launch_match: str = "") -> str:
-    zone = data.get("source", "N/A")
-    time_window = data.get("time_window", "")
-    text = data.get("text", "").replace("*", "\\*").replace("_", "\\_").replace("`", "\\`")
-
     if res_type == "air":
         zone_emoji = "✈️"
     else:
         zone_emoji = "🌊"
+
+    zone = data.get("source", "N/A")
+    time_window = data.get("time_window", "")
+    text = data.get("text", "").replace("*", "\\*").replace("_", "\\_").replace("`", "\\`")
 
     msg = f"{zone_emoji} *{zone}*\n"
 
